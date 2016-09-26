4e81988ba6cb783c7a2a0f78aedeaf3b1dfa4023
Lauren Spiegel <lhspiegel@gmail.com>
2016-09-26 12:53:19 -0700

    lhs changes

--- tests/functional/jaws/src/test/java/com/scality/StreamingAuthV4Test.java
+++ tests/functional/jaws/src/test/java/com/scality/StreamingAuthV4Test.java
@@ -21,35 +21,43 @@ import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.Bucket;
 public class StreamingAuthV4Test {
-	protected static String accessKey ;
-        public String getAccessKey() { return accessKey ; }
-        protected static String secretKey  ;
-        public String getSecretKey() { return secretKey; } 
-        //run before once before all the tests
-	@BeforeClass public static void initAccessKey() throws Exception {
-		JSONParser parser = new JSONParser(); 
-		String path = Paths.get("config.json").toAbsolutePath().toString();
-        	JSONObject obj = (JSONObject) parser.parse(new FileReader(path));
-            	StreamingAuthV4Test.accessKey = (String) obj.get("accessKey");
-            	StreamingAuthV4Test.secretKey = (String) obj.get("secretKey");
-        }
-
-        protected AmazonS3 s3client ;
+	protected static String accessKey;
+	public static  String getAccessKey() { return accessKey; }
+	protected static String secretKey;
+	public static String getSecretKey() { return secretKey; }
+	protected static String transport;
+	public static String getTransport() { return transport; }
+	protected static String ipAddress;
+	public static String getIpAddress() { return ipAddress; }
+	protected static AmazonS3 s3client;
 	public AmazonS3 getS3Client() { return this.s3client; }
-        //runs before every test
-	@Before public void initAWS3Client() throws Exception {
-		BasicAWSCredentials awsCreds = new BasicAWSCredentials(getAccessKey(), getSecretKey());
-                this.s3client = new AmazonS3Client(awsCreds);
-                this.s3client.setEndpoint("http://127.0.0.1:8000");
-                this.s3client.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true));
+
+        //run once before all the tests
+	@BeforeClass public static void initConfig() throws Exception {
+		JSONParser parser = new JSONParser();
+		String path = Paths.get("../config.json").toAbsolutePath().toString();
+		System.out.println(path);
+    	JSONObject obj = (JSONObject) parser.parse(new FileReader(path));
+    	StreamingAuthV4Test.accessKey = (String) obj.get("accessKey");
+    	StreamingAuthV4Test.secretKey = (String) obj.get("secretKey");
+		StreamingAuthV4Test.transport = (String) obj.get("transport");
+		StreamingAuthV4Test.ipAddress = (String) obj.get("ipAddress");
+
+		BasicAWSCredentials awsCreds =
+			new BasicAWSCredentials(getAccessKey(), getSecretKey());
+		s3client = new AmazonS3Client(awsCreds);
+		s3client.setEndpoint(getTransport() + "://" + getIpAddress() +
+			":8000");
+		s3client.setS3ClientOptions(new S3ClientOptions()
+			.withPathStyleAccess(true));
 	}
-	
+
  	@Test public void testBucket() throws Exception {
-                final String bucketName = "somebucket" ;
- 		this.getS3Client().createBucket(bucketName);
-  		Object[] buckets=this.getS3Client().listBuckets().toArray();
+        final String bucketName = "somebucket" ;
+ 		getS3Client().createBucket(bucketName);
+  		Object[] buckets=getS3Client().listBuckets().toArray();
 		Assert.assertEquals(buckets.length,1);
 		Bucket bucket = (Bucket) buckets[0];
 		Assert.assertEquals(bucketName,bucket.getName());
-        }
+    }
 }
