cb4cafda283080d1ed8791873ec1d5a34bab97c8
Electra Chong <electra.chong@scality.com>
2016-11-16 11:30:32 -0800

    Add first functional test
    * Create helper class -- not sure how useful it is, but eh
    * Wrote test to create bucket website config with index doc

---
+++ tests/functional/aws-node-sdk/test/bucket/website.js
@@ -0,0 +1,88 @@
+import assert from 'assert';
+
+import withV4 from '../support/withV4';
+import BucketUtility from '../../lib/utility/bucket-util';
+
+const bucketName = 'testbucketwebsitebucket';
+
+class _makeWebsiteConfig {
+    constructor(indexDocument, errorDocument, redirectAllReqHost,
+        redirectAllReqProtocol) {
+        if (indexDocument) {
+            this.IndexDocument = {};
+            this.IndexDocument.Suffix = indexDocument;
+        }
+        if (errorDocument) {
+            this.ErrorDocument = {};
+            this.ErrorDocument.Key = errorDocument;
+        }
+        if (redirectAllReqHost) {
+            this.RedirectAllRequestTo = {};
+            this.RedirectAllRequestTo.HostName = redirectAllReqHost;
+            if (redirectAllReqProtocol) {
+                this.RedirectAllRequestTo.Protocol = redirectAllReqProtocol;
+            }
+        }
+    }
+    addRoutingRule(redirectParams, conditionParams) {
+        const newRule = {};
+        if (!this.RoutingRules) {
+            this.RoutingRules = [];
+        }
+        if (redirectParams) {
+            newRule.Redirect = {};
+            Object.keys(redirectParams).forEach(key => {
+                newRule.Redirect[`${key}`] = redirectParams.key;
+            });
+        }
+        if (conditionParams) {
+            newRule.Condition = {};
+            Object.keys(conditionParams).forEach(key => {
+                newRule.Condition[`${key}`] = conditionParams.key;
+            });
+        }
+        this.RoutingRules.push(newRule);
+    }
+    // another property to add is MD5...
+}
+
+describe('PUT bucket website', () => {
+    withV4(sigCfg => {
+        const bucketUtil = new BucketUtility('default', sigCfg);
+        const s3 = bucketUtil.s3;
+
+        beforeEach(done => {
+            process.stdout.write('about to create bucket\n');
+            s3.createBucket({ Bucket: bucketName }, err => {
+                if (err) {
+                    process.stdout.write('error in beforeEach', err);
+                    done(err);
+                }
+                done();
+            });
+        });
+
+        afterEach(() => {
+            process.stdout.write('about to empty bucket\n');
+            return bucketUtil.empty(bucketName).then(() => {
+                process.stdout.write('about to delete bucket\n');
+                return bucketUtil.deleteOne(bucketName);
+            }).catch(err => {
+                if (err) {
+                    process.stdout.write('error in afterEach', err);
+                    throw err;
+                }
+            });
+        });
+
+        it.only('should put a bucket website successfully', done => {
+            const config = new _makeWebsiteConfig('index.html');
+            s3.putBucketWebsite({ Bucket: bucketName,
+                WebsiteConfiguration: config }, (err, res) => {
+                assert.strictEqual(err, null, `Found unexpected err ${err}`);
+                console.log(res);
+                done();
+            });
+        });
+    });
+});
